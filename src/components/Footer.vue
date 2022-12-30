a9f3cbe1fb245cc6fc0165ef2d09720fb61d7818
Andrés F. Rodríguez <andresfra65@gmail.com>
2022-12-30 13:06:56 -0500

    update: order footer logic bool

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,42 +1,44 @@
 <template>
-  <div class="footer">
-    <div class="footer__facebook-content">
+  <div 
+  :class="[token === true ? 'footer2' : 'footer1']"
+  >
+    <div class="footer1__facebook-content footer2__facebook-content">
       <img
         @click="gotoSaltin()"
-        class="footer__facebook"
+        class="footer1__facebook footer2__facebook"
         src="@/assets/web/ico_facebook.png"
       />
-      <span class="footer__facebook-text" @click="gotoSaltin()"
+      <span class="footer1__facebook-text footer2__facebook-text" @click="gotoSaltin()"
         >galletassaltinnoel</span
       >
       <img
         @click="gotoDucales()"
-        class="footer__facebook"
+        class="footer1__facebook footer2__facebook"
         src="@/assets/web/ico_facebook.png"
       />
-      <span class="footer__facebook-text" @click="gotoDucales()"
+      <span class="footer1__facebook-text footer2__facebook-text" @click="gotoDucales()"
         >ducalesgruponutresa</span
       >
     </div>
-    <span class="footer__text">Noel - 2023</span>
-    <div class="footer__routes" v-if="token">
-      <img class="footer__coljuegos" src="@/assets/web/Logo_coljuegos.png" />
+    <span class="footer1__text footer2__text">Noel - 2023</span>
+    <div class="footer1__routes footer2__routes" v-if="token">
+      <img class="footer1__coljuegos footer2__coljuegos" src="@/assets/web/Logo_coljuegos.png" />
       <span
         v-if="token"
         style="margin: 20px"
-        class="footer__text cp"
+        class="footer1__text footer2__text cp"
         @click="gotoTerms()"
         >Términos y Condiciones</span
       >
       <span
         v-if="token"
         style="margin: 20px"
-        class="footer__text cp"
+        class="footer1__text footer2__text cp"
         @click="gotoContacts()"
         >Contáctanos</span
       >
     </div>
-    <img v-if="!token" class="footer__coljuegos" src="@/assets/web/Logo_coljuegos.png" />
+    <img v-if="!token" class="footer1__coljuegos footer2__coljuegos" src="@/assets/web/Logo_coljuegos.png" />
   </div>
 </template>
 
@@ -96,8 +98,68 @@ export default {
 
 <style lang="scss">
 @import "@/assets/scss/mixins.scss";
-.footer {
+.footer1 {
   display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 50px;
+  z-index: 3;
+  background-color: #882518;
+  padding: 0 0;
+  @include lg() {
+    height: 60px;
+  }
+  @include xlg() {
+    height: 70px;
+  }
+  &__coljuegos {
+    height: 59.3px;
+  }
+  &__facebook {
+    height: 20px;
+    margin-right: 10px;
+    cursor: pointer;
+  }
+  &__facebook-content {
+    display: flex;
+    align-items: center;
+  }
+  &__facebook-text {
+    color: #f0b824;
+    font-size: 13px;
+    margin-right: 20px;
+    cursor: pointer;
+    @include lg() {
+      font-size: 15px;
+    }
+    @include xlg() {
+      font-size: 22px;
+    }
+  }
+  &__text {
+    font-size: 13px;
+    color: #f0b824;
+    text-align: center;
+    @include lg() {
+      font-size: 15px;
+    }
+    @include xlg() {
+      font-size: 22px;
+    }
+  }
+  .cp {
+    cursor: pointer;
+    text-decoration: underline;
+  }
+  &__routes {
+    display: flex;
+    align-items: center;
+    flex-direction: row-reverse;
+  }
+}
+.footer2 {
+  display: flex;
+  flex-direction: row-reverse;
   justify-content: space-around;
   align-items: center;
   height: 50px;
