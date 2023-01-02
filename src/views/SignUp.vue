8e4f49c66e6b06c1bb602f8060f7b0a49cac1522
Andrés F. Rodríguez <andresfra65@gmail.com>
2023-01-01 23:01:35 -0500

    test register function

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,167 +1,179 @@
 <template>
   <div class="signUp">
-      <div class="back-content" @click="goTo('/ingresar')" v-if="false">
-        <div class="back">
-          <span class="back-text">Volver</span>
-        </div>
+    <div class="back-content" @click="goTo('/ingresar')" v-if="false">
+      <div class="back">
+        <span class="back-text">Volver</span>
       </div>
-      <div class="signUp__content">
-        <div class="">
-          <img
-            v-if="!mobile"
-            class="signUp__title"
-            src="../assets/Assets_Web_New/Titulo_registro_usuarios.png"
-            alt="Como participar titulo"
-          />
-          <img
-            v-if="mobile"
-            class="signUp__title__titleMobile"
-            src="../assets/Assets_Mobile_New/Titulo_registro_usuario.png"
-            alt="Como participar"
-          />
-        </div>
-        <span class="signUp__subtitle">Para participar completa tu registro.</span>
-        <div class="signUp__form-row">
-          <Input
-            :model="user.name"
-            @close-all="
-              $refs.operator.open = false;
-              $refs.department_state.open = false;
-            "
-            field="name"
-            label="Nombres"
-            @handle-input="setValue($event)"
-            placeholder="Ingresar nombre completo"
-            :required="true"
-            :error="errors.name"
-          />
-          <Input
-            :model="user.lastName"
-            @close-all="
-              $refs.operator.open = false;
-              $refs.department_state.open = false;
-            "
-            field="lastName"
-            label="Apellidos"
-            @handle-input="setValue($event)"
-            placeholder="Ingresar apellidos"
-            :required="true"
-            :error="errors.lastnAME"
-          />
-          <Input
+    </div>
+    <div class="signUp__content">
+      <div class="">
+        <img
+          v-if="!mobile"
+          class="signUp__title"
+          src="../assets/Assets_Web_New/Titulo_registro_usuarios.png"
+          alt="Como participar titulo"
+        />
+        <img
+          v-if="mobile"
+          class="signUp__title__titleMobile"
+          src="../assets/Assets_Mobile_New/Titulo_registro_usuario.png"
+          alt="Como participar"
+        />
+      </div>
+      <span class="signUp__subtitle"
+        >Para participar completa tu registro.</span
+      >
+      <div class="signUp__form-row">
+        <Input
+          :model="user.name"
+          @close-all="
+            $refs.operator.open = false;
+            $refs.department_state.open = false;
+          "
+          field="name"
+          label="Nombres"
+          @handle-input="setValue($event)"
+          placeholder="Ingresar nombre completo"
+          :required="true"
+          :error="errors.name"
+        />
+        <Input
+          :model="user.lastName"
+          @close-all="
+            $refs.operator.open = false;
+            $refs.department_state.open = false;
+          "
+          field="lastName"
+          label="Apellidos"
+          @handle-input="setValue($event)"
+          placeholder="Ingresar apellidos"
+          :required="true"
+          :error="errors.lastnAME"
+        />
+        <Input
           label="Cédula"
           field="idn"
           @close-all="
-              $refs.operator.open = false;
-              $refs.department_state.open = false;
-            "
-            :model="user.idn"
-            @handle-input="setValue($event)"
-            placeholder="Ingresa nº de cédula"
-            :required="true"
-            :error="errors.idn"
-            :onlyNumbers="true"
-            />
-          </div>
-          <div class="signUp__form-row">
-          <Input
-            :model="user.email"
-            field="email"
-            @close-all="
-              $refs.operator.open = false;
-              $refs.department_state.open = false;
-            "
-            label="Correo Electrónico"
-            @handle-input="setValue($event)"
-            placeholder="Ingresar correo electrónico"
-            :required="true"
-            :error="errors.email"
-          />
-          <Select
-              ref="department_state"
-              @close-all="$refs.operator.open = false"
-              field="department_state"
-              label="Departamento"
-              :model="user.department_state"
-              placeholder="Seleccionar departamento"
-              :items="fomattedStates"
-              @handle-input="setValue($event)"
-              :required="true"
-              :error="errors.department_state"
-            />
-            <Select
-                ref="city"
-                field="city"
-                label="Ciudad"
-                :model="user.city"
-                placeholder="Seleccionar Ciudad"
-                :items="formattedCities"
-                @handle-input="setValue($event)"
-                :required="true"
-                :error="errors.city"
-            />
-          </div>
-          <div class="signUp__form-row">
-            <Input
-              field="phone"
-              label="Celular"
-              :model="user.phone"
-              @close-all="
-                $refs.operator.open = false;
-                $refs.department_state.open = false;
-              "
-              @handle-input="setValue($event)"
-              placeholder="Ingresa nº celular"
-              :required="true"
-              :error="errors.phone"
-              maxlength="10"
-              :onlyNumbers="true"
-            />
-            <Select
-              ref="operator"
-              field="operator"
-              @close-all="$refs.department_state.open = false"
-              label="Operador"
-              :model="user.operator"
-              placeholder="Seleccionar operador"
-              :items="fomattedOperators"
-              @handle-input="setValue($event)"
-              :required="true"
-              :error="errors.operator"
-            />
-            <Select
-              style="opacity: 0;"
-              ref="operator"
-              field="operator"
-              @close-all="$refs.department_state.open = false"
-              label="Operador"
-              :model="user.operator"
-              placeholder="Seleccionar operador"
-              :items="fomattedOperators"
-              @handle-input="setValue($event)"
-              :required="true"
-              :error="errors.operator"
-            />
-          </div>
-          <div class="signUp__radioBigBox" >
-            <div class="signUp__radio-container">
-              <div>
-                <Radio @handle-click="toggleTerms()" :value="terms"/>
-              </div>
-              <span class="signUp__radio-text">Acepto los <span class="signUp__radio-text-action" @click="goToTerms()">términos y condiciones</span>.</span>
-            </div>
-            <div class="signUp__radio-container">
-              <div style="width:40px;">
-                <div>
-                  <Radio @handle-click="toggleTerms4()" :value="terms4"/>
-                </div>
-              </div>
-              <span class="signUp__radio-text" style="margin-left: -12px;">Acepto las <span class="signUp__radio-text-action" @click="goToPolo()">políticas de privacidad</span>.</span>
-            </div>
-          </div>
-          <Button text="Registrarme" type="primary" @handle-click="send()"/>
+            $refs.operator.open = false;
+            $refs.department_state.open = false;
+          "
+          :model="user.idn"
+          @handle-input="setValue($event)"
+          placeholder="Ingresa nº de cédula"
+          :required="true"
+          :error="errors.idn"
+          :onlyNumbers="true"
+        />
       </div>
-     <modal :dialog="dialog" @close="dialog = false" width="600">
+      <div class="signUp__form-row">
+        <Input
+          :model="user.email"
+          field="email"
+          @close-all="
+            $refs.operator.open = false;
+            $refs.department_state.open = false;
+          "
+          label="Correo Electrónico"
+          @handle-input="setValue($event)"
+          placeholder="Ingresar correo electrónico"
+          :required="true"
+          :error="errors.email"
+        />
+        <Select
+          ref="department_state"
+          @close-all="$refs.operator.open = false"
+          field="department_state"
+          label="Departamento"
+          :model="user.department_state"
+          placeholder="Seleccionar departamento"
+          :items="fomattedStates"
+          @handle-input="setValue($event)"
+          :required="true"
+          :error="errors.department_state"
+        />
+        <Select
+          ref="city"
+          field="city"
+          label="Ciudad"
+          :model="user.city"
+          placeholder="Seleccionar Ciudad"
+          :items="formattedCities"
+          @handle-input="setValue($event)"
+          :required="true"
+          :error="errors.city"
+        />
+      </div>
+      <div class="signUp__form-row">
+        <Input
+          field="phone"
+          label="Celular"
+          :model="user.phone"
+          @close-all="
+            $refs.operator.open = false;
+            $refs.department_state.open = false;
+          "
+          @handle-input="setValue($event)"
+          placeholder="Ingresa nº celular"
+          :required="true"
+          :error="errors.phone"
+          maxlength="10"
+          :onlyNumbers="true"
+        />
+        <Select
+          ref="operator"
+          field="operator"
+          @close-all="$refs.department_state.open = false"
+          label="Operador"
+          :model="user.operator"
+          placeholder="Seleccionar operador"
+          :items="fomattedOperators"
+          @handle-input="setValue($event)"
+          :required="true"
+          :error="errors.operator"
+        />
+        <Select
+          style="opacity: 0"
+          ref="operator"
+          field="operator"
+          @close-all="$refs.department_state.open = false"
+          label="Operador"
+          :model="user.operator"
+          placeholder="Seleccionar operador"
+          :items="fomattedOperators"
+          @handle-input="setValue($event)"
+          :required="true"
+          :error="errors.operator"
+        />
+      </div>
+      <div class="signUp__radioBigBox">
+        <div class="signUp__radio-container">
+          <div>
+            <Radio @handle-click="toggleTerms()" :value="terms" />
+          </div>
+          <span class="signUp__radio-text"
+            >Acepto los
+            <span class="signUp__radio-text-action" @click="goToTerms()"
+              >términos y condiciones</span
+            >.</span
+          >
+        </div>
+        <div class="signUp__radio-container">
+          <div style="width: 40px">
+            <div>
+              <Radio @handle-click="toggleTerms4()" :value="terms4" />
+            </div>
+          </div>
+          <span class="signUp__radio-text" style="margin-left: -12px"
+            >Acepto las
+            <span class="signUp__radio-text-action" @click="goToPolo()"
+              >políticas de privacidad</span
+            >.</span
+          >
+        </div>
+      </div>
+      <Button text="Registrarme" type="primary" @handle-click="send()" />
+    </div>
+    <modal :dialog="dialog" @close="dialog = false" width="600">
       <signup-confirm
         v-if="dialog"
         :user="user"
@@ -174,15 +186,16 @@
 </template> 
 
 <script>
-import Radio  from '../components/Radio'
-import Button  from '../components/Button'
-import Input from '../components/Input'
-import Select from '../components/Select'
-import Modal from '../components/Modal'
-import SignupConfirm from '../components/SignUpConfirm'
+import Radio from "../components/Radio";
+import Button from "../components/Button";
+import Input from "../components/Input";
+import Select from "../components/Select";
+import Modal from "../components/Modal";
+import SignupConfirm from "../components/SignUpConfirm";
+import json from "../assets/json/colombia.json";
 
 export default {
-  name: 'SignUp',
+  name: "SignUp",
   data() {
     return {
       terms: false,
@@ -200,7 +213,7 @@ export default {
         "Avantel",
         "ETB",
         "Flash Mobile",
-        "Móvil Éxito"
+        "Móvil Éxito",
       ],
       states: [
         "AMAZONAS",
@@ -235,8 +248,10 @@ export default {
         "SAN ANDRES",
         "PUTUMAYO",
         "VAUPÉS",
-        "VICHADA"
-      ]
+        "VICHADA",
+      ],
+      list_cities: [],
+      data_colombia: json,
     };
   },
   mounted() {},
@@ -246,20 +261,26 @@ export default {
     Input,
     Select,
     Modal,
-    SignupConfirm
+    SignupConfirm,
   },
   props: {},
   computed: {
     fomattedOperators() {
-      return this.operators.map(o => ({
+      return this.operators.map((o) => ({
         value: o,
-        text: o
+        text: o,
       }));
     },
     fomattedStates() {
-      return this.states.map(s => ({
+      return this.states.map((s) => ({
         value: s,
-        text: s
+        text: s,
+      }));
+    },
+    formattedCities() {
+      return this.list_cities.map((s) => ({
+        value: s,
+        text: s,
       }));
     },
     mobile() {
@@ -271,10 +292,17 @@ export default {
   },
   methods: {
     toggleTerms() {
-      this.terms = !this.terms
+      this.terms = !this.terms;
+    },
+    SetCities(department) {
+      this.data_colombia.forEach((c) => {
+        if (c.departamento == department) {
+          this.list_cities = c.ciudades;
+        }
+      });
     },
     toggleTerms4() {
-      this.terms4 = !this.terms4
+      this.terms4 = !this.terms4;
     },
     send() {
       if (
@@ -289,39 +317,41 @@ export default {
         this.validate();
         this.$store.dispatch("setAlert", {
           buttonLabel: "Aceptar",
-          type:'INFO',
+          type: "INFO",
           showClose: true,
-          message: "¡Debes completar los campos requeridos!."
+          message: "¡Debes completar los campos requeridos!.",
         });
       } else {
         if (!Object.keys(this.errors).length) {
           if (!this.terms) {
             this.$store.dispatch("setAlert", {
               buttonLabel: "Aceptar",
-              type:'INFO',
+              type: "INFO",
               showClose: true,
-              message: "¡Debes aceptar los términos y condiciones de la promoción!."
+              message:
+                "¡Debes aceptar los términos y condiciones de la promoción!.",
             });
           } else if (!this.terms4) {
             this.$store.dispatch("setAlert", {
               buttonLabel: "Aceptar",
-              type:'INFO',
+              type: "INFO",
               showClose: true,
-              message: "¡Debes aceptar las políticas de privacidad de la promoción!."
+              message:
+                "¡Debes aceptar las políticas de privacidad de la promoción!.",
             });
-          }else {
+          } else {
             this.dialog = true;
           }
         } else {
           const e = [];
-          Object.keys(this.errors).forEach(key => {
+          Object.keys(this.errors).forEach((key) => {
             e.push(this.errors[key]);
-          })
+          });
           this.$store.dispatch("setAlert", {
             buttonLabel: "Aceptar",
-            type:'INFO',
+            type: "INFO",
             showClose: true,
-            messages: e
+            messages: e,
           });
         }
       }
@@ -364,7 +394,8 @@ export default {
       let errors = {};
       const emailReg = /^[a-zA-Z0-9_\-\.]{5,30}@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
       const idReq = /^([1-9]{1}[0-9]{6,9})$/;
-      const phoneReq = /^(300|301|302|304|305|310|311|312|313|314|315|316|317|318|319|320|321|322|323|324|350|351){1}[0-9]{1}[0-9]{6}$/;
+      const phoneReq =
+        /^(300|301|302|304|305|310|311|312|313|314|315|316|317|318|319|320|321|322|323|324|350|351){1}[0-9]{1}[0-9]{6}$/;
       if (this.user.email && !emailReg.test(this.user.email)) {
         errors.email = "Ingresa un correo válido.";
       }
@@ -399,8 +430,8 @@ export default {
       this.errors = errors;
     },
   },
-  watch: {}
-}
+  watch: {},
+};
 </script>
 
 <style lang="scss">
@@ -413,17 +444,17 @@ export default {
   align-items: center;
   width: 80%;
   padding: 10px 0;
-  
+
   @include mobile() {
     flex-direction: column;
     align-items: center;
     padding: 10px 0 70px 0;
     width: 90%;
   }
-  &__title{
+  &__title {
     margin: 2% 0 0 0;
     height: 60px;
-    @include mnHeight(1000px){
+    @include mnHeight(1000px) {
       height: 8vh;
     }
     &__titleMobile {
@@ -447,7 +478,7 @@ export default {
     align-items: center;
     margin-bottom: 4px;
     justify-content: flex-start;
-     cursor: pointer;
+    cursor: pointer;
   }
   &__icon {
     height: 35px;
@@ -459,7 +490,7 @@ export default {
   }
   &__box {
     margin-top: 6px;
-    background-color: #FFD800;
+    background-color: #ffd800;
     border-radius: 30px;
     padding: 6px 30px;
     display: flex;
